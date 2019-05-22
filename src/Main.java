3f6cda7a0854a0387074db494d257033af81e32a
carlos <cgarciapieto@gmail.com>
2019-05-22 15:43:07 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Main {
 
-    private static HashMap<String, String> Qs = new HashMap<String, String>();
+    private static HashMap<String, Integer> Qs = new HashMap<String, Integer>();
 
     public static void main(String[] args) {
         File myFile = new File("/home/student/CodeSchool/IdeaProjects/Java/2019-05-22/java-mini-project-quizzer-cgarciapieto/quizzer.txt");
@@ -15,8 +15,8 @@ public class Main {
             myReader = new Scanner(myFile);
             while (myReader.hasNextLine()){
                 String txt = myReader.nextLine();
-                String[] parts = txt.split(",");
-//                Qs.put(parts[0], parts[3]);
+                String[] x = txt.split(",");
+//                Qs.put(x[0], x[3]);
             }
 
         }
@@ -27,7 +27,7 @@ public class Main {
         Scanner reader = new Scanner(System.in);
         System.out.print("What is the answer");
         String Question = reader.nextLine().trim().toLowerCase();
-        String response = Qs.get(Question) != null ? Qs.get(Question) : "Invalid answer";
+        Integer response = Qs.get(Question) != null ? Qs.get(Question) : 0;
         System.out.println(response);
 
 
